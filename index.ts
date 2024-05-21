6cf46ff54443a2a8c7856877529a33cda2b5a685
Syed Konain Nasir <konain611@gmail.com>
2024-05-21 20:38:52 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+//SHABANG
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {
+    message: "Enter First Number:",
+    type: "number",
+    name: "firstNumber"
+    },
+    {
+    message: "Enter Second Number:",
+    type: "number",
+    name: "secondNumber"
+    },
+    {
+    message: "Select any operator to perform action",
+    type: "list",
+    name: "Operator",
+    choices: ["Addition" , "Subtraction" , "Multiplication" , "Division"],
+    },
+    
+]);
+
+if (answer.Operator === "Addition"){
+    console.log(`Your answer is ${answer.firstNumber+answer.secondNumber}.`);
+}
+else if (answer.Operator === "Subtraction"){
+    console.log(`Your answer is ${answer.firstNumber-answer.secondNumber}.`);
+}
+else if (answer.Operator === "Multiplication"){
+    console.log(`Your answer is ${answer.firstNumber*answer.secondNumber}.`);
+}
+else if (answer.Operator === "Division"){
+    console.log(`Your answer is ${answer.firstNumber/answer.secondNumber}.`);
+}
+else{
+    console.log("Please select valid operator");
+    
+}
